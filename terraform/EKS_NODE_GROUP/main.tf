056681cb1188932e93878b267da818d85076f291
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 12:45:32 +0100

    added eks node group module

---
+++ terraform/EKS_NODE_GROUP/main.tf
@@ -0,0 +1,22 @@
+# eks node group / main
+
+resource "aws_eks_node_group" "eks_node_group" {
+  instance_type   = var.instance_type
+  ami_type        = var.ami_type
+  cluster_name    = var.cluster_name
+  node_group_name = var.node_group_name
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  depends_on = var.eks_node_group_depends_on
+
+  tags = {
+    "Name" = var.name_tag
+  }
+}
