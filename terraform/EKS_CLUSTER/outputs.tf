7f86e19785ec5251261a8611c1b9c6f1f3dc2034
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 17:00:35 +0100

    added eks cluster output values and certificate authority data

---
+++ terraform/EKS_CLUSTER/outputs.tf
@@ -0,0 +1,10 @@
+# eks cluster / outputs
+
+# required by jenkins to run kubectl commands.
+# data - The base64 encoded certificate data required to communicate with your cluster.
+# Add this to the certificate-authority-data section of the kubeconfig file for your cluster.
+output "certificate_authority_data" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority.data
+}
+# need to oyutput the kubeconfig file for the jenkins user so it can connect
+#(or i could make the jenkins user the admin?)
