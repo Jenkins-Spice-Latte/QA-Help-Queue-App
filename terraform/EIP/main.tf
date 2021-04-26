de7e4091781007906598c37155ddebd8cef503a8
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 16:31:04 +0100

    added depends on variable in eip

--- terraform/EIP/main.tf
+++ terraform/EIP/main.tf
@@ -4,6 +4,8 @@ resource "aws_eip" "eip" {
   instance = var.instance
   vpc      = var.vpc
 
+  depends_on = var.eip_depends_on
+
   tags = {
     "Name" = var.name_tag
   }
--- terraform/EIP/variables.tf
+++ terraform/EIP/variables.tf
@@ -8,6 +8,10 @@ variable "instance" {
   description = "EC2 instance ID."
 }
 
+variable "eip_depends_on" {
+  description = "Usually depends on iam role policies."
+}
+
 variable "name_tag" {
   description = "Name tag to assign to the resource"
   type        = string
