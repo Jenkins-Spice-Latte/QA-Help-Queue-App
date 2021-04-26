08551702254e88f3a6fca65323a0f66296722143
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 17:29:08 +0100

    cleaned up main and variables root files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,36 @@
 # root / main
+
+module "VPC" {
+  source = "./VPC"
+
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  name_tag             = "hq_vpc"
+}
+
+module "INTERNET_GATEWAY" {
+  source = "./INTERNET_GATEWAY"
+
+  vpc_id = module.VPC.vpc_id
+  name_tag = "hq_internet_gateway"
+}
+module "PUBLIC_SUBNET" {
+  source = "./SUBNET"
+
+  vpc_id                  = module.VPC.vpc_id
+  availability_zone       = "eu-west-2a"
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  name_tag                = "hq_public_subnet"
+}
+
+module "TEST_PRIVATE_SUBNET" {
+  source = "./SUBNET"
+
+  vpc_id                  = module.VPC.vpc_id
+  availability_zone       = "eu-west-2a"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+  name_tag                = "hq_test_private_subnet"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+# root / variables
+
+# ^ aws info
+variable "aws_region" {
+  description = "London region."
+  default     = "eu-west-2"
+}
+
+# ^ instance info
+variable "ec2_ami_id" {
+  description = "Ubuntu 20.04 in London region."
+  default     = "ami-096cb92bb3580c759"
+}
+
+variable "ec2_instance_type" {
+  description = "Free tier instance type."
+  default     = "t2.micro"
+}
+
+# ^ gitignored tfvars
+variable "aws_access_key" {
+  description = "AWS key for permission to provision resources."
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS key for permission to provision resources."
+  sensitive   = true
+}
