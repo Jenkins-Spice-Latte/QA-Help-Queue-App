b7eb7a1a8f9f208d714727a2fc6b8439dbd38823
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-26 17:13:52 +0100

    added service

--- backend/help-queue/src/main/java/com/qa/helpQueue/service/HelpQueueService.java
+++ backend/help-queue/src/main/java/com/qa/helpQueue/service/HelpQueueService.java
@@ -1,5 +1,61 @@
 package com.qa.helpQueue.service;
 
-public class HelpQueueService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.helpQueue.persistance.domain.Tickets;
+import com.qa.helpQueue.persistance.repository.hqRepository;
+
+@Service
+public class HelpQueueService {
+	
+	private hqRepository repo;
+	
+	@Autowired
+	public HelpQueueService(hqRepository repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	public Tickets create(Tickets ticket) {
+		Tickets created = this.repo.save(ticket);
+		return created;
+	}
+	
+	public List<Tickets> readAll() {
+		List<Tickets> allTickets = this.repo.findAll();
+		return allTickets;
+	}
+	
+	public Tickets readById(Long ticketId) {
+		Tickets ticketById = this.repo.findById(ticketId).orElseThrow();
+		return ticketById;
+	}
+	
+	public Tickets update(Long ticketId, Tickets ticket) {
+		Tickets ticketToUpdate = this.repo.findById(ticketId).orElseThrow();
+
+		ticketToUpdate.setAuthor(ticket.getAuthor());
+		ticketToUpdate.setDescription(ticket.getDescription());
+		ticketToUpdate.setTopic(ticket.getTopic());
+		ticketToUpdate.setUrgency(ticket.getUrgency());
+		ticketToUpdate.setComplete(ticket.isComplete());
+		
+		return this.repo.save(ticketToUpdate);
+	}
+	
+	public boolean delete(Long ticketId) {
+		this.repo.deleteById(ticketId);
+		return !this.repo.existsById(ticketId);
+	}
+	
+	
 }
+
+
+
+
+
+
