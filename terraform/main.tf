d27b69faf6877552ae1798915bfbc1244212a26d
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 20:03:32 +0100

    added eks resources modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
 # root / main
 
+# ^ general
 module "VPC" {
   source = "./VPC"
 
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
+
   name_tag             = "hq_vpc"
 }
 
@@ -16,19 +18,58 @@ module "INTERNET_GATEWAY" {
   name_tag = "hq_internet_gateway"
 }
 
-module "PUBLIC_SUBNET" {
+# ^ deployment resources (eks)
+module "EKS_PUBLIC_SUBNET_A" {
   source = "./SUBNET"
-
   vpc_id                  = module.VPC.vpc_id
   availability_zone       = "eu-west-2a"
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.22.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    "name" = "hq_public_subnet"
+    Name = "hq_eks_public_subnet_a"
+    # tags for kubernetes
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb" = 1
   }
 }
 
+module "EKS_PUBLIC_SUBNET_B" {
+  source = "./SUBNET"
+  vpc_id                  = module.VPC.vpc_id
+  availability_zone       = "eu-west-2b"
+  cidr_block              = "10.0.44.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "hq_eks_public_subnet_b"
+    # tags for kubernetes load balancer
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+module "EIP_NGW" {
+  source = "./EIP"
+
+  eip_depends_on = module.INTERNET_GATEWAY
+
+  name_tag = "hq_eip_for_nat_gateway"
+  instance = ""
+  vpc = null
+}
+
+module "NAT_GATEWAY" {
+  source = "./NAT_GATEWAY"
+
+  allocation_id = module.EIP_NGW.id
+  subnet_id = module.EKS_PUBLIC_SUBNET_A.id
+
+  name_tag = "hq_eks_nat"
+  nat_gateway_depends_on = module.EKS_PUBLIC_SUBNET_A
+}
+
+# ^ CI resources
 module "TEST_PRIVATE_SUBNET" {
   source = "./SUBNET"
 
@@ -38,6 +79,6 @@ module "TEST_PRIVATE_SUBNET" {
   map_public_ip_on_launch = false
 
   tags = {
-    "name" = "hq_test_private_subnet"
+    Name = "hq_test_private_subnet"
   }
 }
