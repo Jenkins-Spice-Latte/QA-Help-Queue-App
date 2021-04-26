5a1a44e148ffea2a338af4f64d552c8a0f811735
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 22:16:59 +0100

    added cidr block and gateway id

--- terraform/RT/main.tf
+++ terraform/RT/main.tf
@@ -3,6 +3,11 @@
 resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
 
+  route {
+    cidr_block = var.route_cidr_block
+    gateway_id = var.gateway_id
+  }
+
   tags = {
     "Name" = var.name_tag
   }
--- terraform/RT/variables.tf
+++ terraform/RT/variables.tf
@@ -4,6 +4,14 @@ variable "vpc_id" {
   description = "The VPC ID."
 }
 
+variable "route_cidr_block" {
+  description = "The CIDR block of the route."
+}
+
+variable "gateway_id" {
+  description = "Identifier of a VPC internet gateway or a virtual private gateway."
+}
+
 variable "name_tag" {
   description = "Name tag to assign to the resource"
   type        = string
