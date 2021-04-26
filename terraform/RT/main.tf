b21b2f6fc2aa07dbca73fb071e6fb9099284884b
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 12:44:09 +0100

    added route table module

---
+++ terraform/RT/main.tf
@@ -0,0 +1,10 @@
+# rt / main
+
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    "Name" = var.name_tag
+  }
+}
+
---
+++ terraform/RT/variables.tf
@@ -0,0 +1,10 @@
+# rt / variables
+
+variable "vpc_id" {
+  description = "The VPC ID."
+}
+
+variable "name_tag" {
+  description = "Name tag to assign to the resource"
+  type        = string
+}
