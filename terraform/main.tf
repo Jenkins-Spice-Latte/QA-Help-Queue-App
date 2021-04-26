1a6c668eb44ca5418d4da4462f9478b632abf055
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-26 18:54:12 +0100

    using object tags instead of string

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,10 @@ module "VPC" {
 module "INTERNET_GATEWAY" {
   source = "./INTERNET_GATEWAY"
 
-  vpc_id = module.VPC.vpc_id
+  vpc_id   = module.VPC.vpc_id
   name_tag = "hq_internet_gateway"
 }
+
 module "PUBLIC_SUBNET" {
   source = "./SUBNET"
 
@@ -22,7 +23,10 @@ module "PUBLIC_SUBNET" {
   availability_zone       = "eu-west-2a"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  name_tag                = "hq_public_subnet"
+
+  tags = {
+    "name" = "hq_public_subnet"
+  }
 }
 
 module "TEST_PRIVATE_SUBNET" {
@@ -32,5 +36,8 @@ module "TEST_PRIVATE_SUBNET" {
   availability_zone       = "eu-west-2a"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = false
-  name_tag                = "hq_test_private_subnet"
+
+  tags = {
+    "name" = "hq_test_private_subnet"
+  }
 }
