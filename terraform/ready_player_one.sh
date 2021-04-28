ee063daac53decca799b479aa831430f99516237
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-28 13:21:08 +0100

    init script

--- terraform/ready_player_one.sh
+++ terraform/ready_player_one.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install unzip
+
+# getting terraform zip
+if [ ! -f terraform_0.15.1_linux_amd64.zip ]
+then
+	wget https://releases.hashicorp.com/terraform/0.15.1/terraform_0.15.1_linux_amd64.zip
+fi
+
+unzip terraform_*_linux_*.zip
+cd terraform_0.15.1_linux_amd64
+
+
+# applying terraform binary
+if [ ! -f /usr/local/bin/terraform ]
+then
+	sudo mv terraform /usr/local/bin
+fi
+
+terraform --version
