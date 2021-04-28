e97f1b6e95cf12210cb9bbc7b006c4ca70b18314
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-28 13:03:43 +0100

    unit tests for createTicket service

--- backend/CreateTicket/src/test/java/com/qa/helpQueue/service/createServiceUnitTest.java
+++ backend/CreateTicket/src/test/java/com/qa/helpQueue/service/createServiceUnitTest.java
@@ -1,5 +1,35 @@
 package com.qa.helpQueue.service;
 
-public class createServiceUnitTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.helpQueue.persistance.domain.Tickets;
+import com.qa.helpQueue.persistance.repository.hqRepository;
+
+@SpringBootTest
+public class CreateServiceUnitTest {
+	
+	@Autowired
+	private CreateService service;
+	
+	@MockBean
+	private hqRepository repo;
+	
+	private final Tickets TEST_TICKET_1 = new Tickets("Terraform bug", "Sonny", "Not applying correctly", 2452345L, "Terraform", 5L, false);
+	
+	@Test
+	void createTest() throws Exception {
+		when(this.repo.save(TEST_TICKET_1)).thenReturn(TEST_TICKET_1);
+		assertThat(this.service.create(TEST_TICKET_1)).isEqualTo(TEST_TICKET_1);
+		verify(this.repo, atLeastOnce()).save(TEST_TICKET_1);
+	}
+	
 
 }
