f4a52db002f19086943b9d576be3ffbbbea6b1bd
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-29 15:41:28 +0100

    added crossorigin to update

--- backend/UpdateTicket/src/main/java/com/qa/helpQueue/rest/controller/UpdateController.java
+++ backend/UpdateTicket/src/main/java/com/qa/helpQueue/rest/controller/UpdateController.java
@@ -4,6 +4,7 @@ package com.qa.helpQueue.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.qa.helpQueue.service.UpdateService;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin
 @RestController
 public class UpdateController {
 	
