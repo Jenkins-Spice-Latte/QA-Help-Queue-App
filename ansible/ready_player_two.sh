ce55962b66268e7b7b7e65eb8811ec05db4b610c
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-29 11:04:25 +0100

    Update ready_player_two.sh

--- ansible/ready_player_two.sh
+++ ansible/ready_player_two.sh
@@ -8,26 +8,17 @@ echo "${bastion_public_ip}"
 echo "${jenkins_public_ip}"
 echo "${testvm_private_ip}"
 
-
 destFile=~/QA-Help-Queue-App/ansible/inventory.yaml
 if [ -f "$destFile" ]; then
-    rm $destFile
+  rm $destFile
 fi
 
 touch $destFile
 echo "
 all:
   hosts:
-    3.9.135.38:
-    18.132.14.178:
-  vars:
-    ansible_user: ubuntu
-    ansible_ssh_private_key_file: \"/home/ubuntu/.ssh/i_dont_give_a_ssh\"
-    ansible_ssh_common_args: \"-o StrictHostKeyChecking=no\"
-
-jenkins:
-  hosts:
-    18.133.181.216:
+    ${bastion_public_ip}:
+    ${jenkins_public_ip}:
   vars:
     ansible_user: ubuntu
     ansible_ssh_private_key_file: \"/home/ubuntu/.ssh/i_dont_give_a_ssh\"
@@ -35,9 +26,17 @@ jenkins:
 
 bastion:
   hosts:
-    18.134.198.195:
+     ${bastion_public_ip}:
   vars:
     ansible_user: ubuntu
     ansible_ssh_private_key_file: \"/home/ubuntu/.ssh/i_dont_give_a_ssh\"
     ansible_ssh_common_args: \"-o StrictHostKeyChecking=no\"
-" > $destFile
+
+jenkins:
+  hosts:
+    ${jenkins_public_ip}:
+  vars:
+    ansible_user: ubuntu
+    ansible_ssh_private_key_file: \"/home/ubuntu/.ssh/i_dont_give_a_ssh\"
+    ansible_ssh_common_args: \"-o StrictHostKeyChecking=no\"
+" >$destFile
