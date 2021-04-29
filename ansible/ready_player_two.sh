cb64c5d8d2fc85e840e89c5ff18fa6711071b5dd
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-29 10:52:10 +0100

    creating inventory.yaml

---
+++ ansible/ready_player_two.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+bastion_public_ip=$(cd ~/QA-Help-Queue-App/terraform || exit ; terraform output bastion_public_ip | tr -d '"')
+jenkins_public_ip=$(cd ~/QA-Help-Queue-App/terraform || exit ; terraform output jenkins_public_ip | tr -d '"')
+testvm_private_ip=$(cd ~/QA-Help-Queue-App/terraform || exit ; terraform output testvm_private_ip | tr -d '"')
+
+echo "${bastion_public_ip}"
+echo "${jenkins_public_ip}"
+echo "${testvm_private_ip}"
+
+
+destFile=~/QA-Help-Queue-App/ansible/inventory.yaml
+if [ -f "$destFile" ]; then
+    rm $destFile
+fi
+
+touch $destFile
+echo "
+all:
+  hosts:
+    ${bastion_public_ip}:
+    ${jenkins_public_ip}:
+  vars:
+    ansible_user: ubuntu
+    ansible_ssh_private_key_file: \"/home/ubuntu/.ssh/i_dont_give_a_ssh\"
+    ansible_ssh_common_args: \"-o StrictHostKeyChecking=no\"
+" > $destFile
