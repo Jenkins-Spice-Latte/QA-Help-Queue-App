a600016a0842963104fc999b37273dbfc821d194
MP <example>
2021-04-30 12:08:28 +0100

    Move to parent

--- Frontend/React/queue/src/App.js
+++ Frontend/React/queue/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { Button } from 'reactstrap';
+import React, { useState } from 'react';
 import Sort from './Components/Sort';
 import Filter from './Components/Filter';
 import Create from './Components/Create';
 import Queue from './Components/Queue';
 import Toggle from './Components/Toggle';
 
+
 function App() {
+  const [modeSelect, setmodeSelect] = useState("Trainee mode");
+  const onCheckboxBtnClick = (selected) => {
+    setmodeSelect(selected);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +20,7 @@ function App() {
       <div className="all_columns">
         <div className="column_one">
           <div className= "create_ticket_div">  
-            <Create/>
+            <Create mode={(modeSelect)}/>
           </div>
           <div  className= "sort_div">
             <Sort/>
@@ -25,10 +31,11 @@ function App() {
         </div>
         <div className="column_two">
           <div className= "toggle_div">
-            <Toggle/>
+            {modeSelect}
+            <Toggle onCheckboxBtnClick={onCheckboxBtnClick} mode={modeSelect}/>
           </div>
           
-            <Queue/>
+            <Queue mode={(modeSelect)}/>
           
         </div>
       </div>
--- Frontend/React/queue/src/Components/Toggle.jsx
+++ Frontend/React/queue/src/Components/Toggle.jsx
@@ -8,22 +8,18 @@ const Toggle = (props) => {
   const [modeSelect, setmodeSelect] = useState("Trainee mode");
   var mode;
 
-  const onCheckboxBtnClick = (selected) => {
-    setmodeSelect(selected);
-  }
 
-  if(modeSelect === "Trainee mode"){
-    mode = <Button color="primary" onClick={() => onCheckboxBtnClick("Trainer mode")}><BsToggleOff className="trainerBtn"/> Use trainer mode</Button>
+
+  if(props.mode === "Trainee mode"){
+    mode = <Button color="primary" onClick={() => props.onCheckboxBtnClick("Trainer mode")}><BsToggleOff className="trainerBtn"/> Use trainer mode</Button>
   } else{
-    mode = <Button color="primary" onClick={() => onCheckboxBtnClick("Trainee mode")}><BsToggleOn className="traineeBtn"/> Use trainee mode</Button>    
+    mode = <Button color="primary" onClick={() => props.onCheckboxBtnClick("Trainee mode")}><BsToggleOn className="traineeBtn"/> Use trainee mode</Button>    
   } 
 
 
     return (
       <> 
         {mode}
-        <Queue mode={(modeSelect)}/>
-        <Create mode={(modeSelect)}/>
       </>
     );
   };
