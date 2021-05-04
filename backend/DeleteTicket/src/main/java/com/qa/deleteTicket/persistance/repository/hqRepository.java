58223c97f359b20ca9078a99c7d7d6a1354e1cff
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-05-04 11:52:53 +0100

    fix errors

--- backend/DeleteTicket/src/main/java/com/qa/deleteTicket/persistance/repository/hqRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qa.deleteTicket.persistance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qa.deleteTicket.persistance.domain.Tickets;
-
-public interface hqRepository extends JpaRepository<Tickets, Long> {
-
-}
