29086417f883cfea71f8fbe7ea430953c006c57b
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-05-06 10:40:21 +0100

    .

--- backend/CreateTicket/src/test/java/com/qa/createTicket/CreateTicketApplicationTests.java
+++ backend/CreateTicket/src/test/java/com/qa/createTicket/CreateTicketApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-//	@ActiveProfiles(profiles = "test")
 class CreateTicketApplicationTests {
 
 	@Test
