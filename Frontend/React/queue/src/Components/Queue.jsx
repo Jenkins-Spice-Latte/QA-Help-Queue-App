17c69b3fda219beda21796353e519c21bcd16b3d
MP <example>
2021-05-06 14:55:56 +0100

    Snapshot testing with Queue.test.jsx, removed other tests

--- Frontend/React/queue/src/Components/Queue.jsx
+++ Frontend/React/queue/src/Components/Queue.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import Ticket from './Ticket'
+import Ticket from './Ticket';
 
 const Queue = (props) => {
   const {className} = props;
--- Frontend/React/queue/src/Testing/Queue.test.jsx
+++ Frontend/React/queue/src/Testing/Queue.test.jsx
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import Queue from '../Components/Queue';
+import renderer from 'react-test-renderer';
+import Ticket from '../Components/Ticket';
 
-jest.mock("axios");
-
-test("should fetch tickets", () => {
-  const tickets = [{  }];
-  const resp = { data: tickets };
-  axios.get.mockResolvedValue(resp);
-
-  return Queue.axios.get("http://localhost:8902/readAll").then((data) => expect(data).toEqual(tickets));
+test('tests to see if Ticket renders properly', () => {
+  let ticket = {
+    ticket_id: 1,
+    author: "auth",
+    complete: true,
+    description: "description",
+    time_created: 101551120,
+    title: "title",
+    topic: "Topic1",
+    urgency: 1
+  };
+  const TicketComp = renderer.create(<Ticket item={ticket} className={Queue.className} sort="Oldest" mode="Trainee mode" switchLoaded={false} isLoaded={false}/>).toJSON();
+  expect(TicketComp).toMatchSnapshot();
 });
